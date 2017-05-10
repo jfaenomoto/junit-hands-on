56081180828dc12d38f79146b33f1d97853853ba
Joao Enomoto <joao@guiabolso.com.br>
2017-05-10 15:05:18 -0300

    - versão inicial do hands-on de JUnit;

---
+++ src/main/java/br/com/guiabolso/junit/PokemonProfessor.java
@@ -0,0 +1,26 @@
+package br.com.guiabolso.junit;
+
+public class PokemonProfessor {
+
+    int contador;
+
+    public PokemonProfessor() {
+        this.contador = 0;
+    }
+
+    String getPokemon() {
+        if (contador > 5) {
+            throw new IllegalStateException("no more pokemon ;(");
+        }
+        String pokemon;
+        if (contador % 3 == 0) {
+            pokemon = "Bulbasaur";
+        } else if (contador % 3 == 1) {
+            pokemon = "Squirtle";
+        } else {
+            pokemon = "Charmander";
+        }
+        this.contador++;
+        return pokemon;
+    }
+}
---
+++ src/test/java/br/com/guiabolso/junit/PokemonProfessorTest.java
@@ -0,0 +1,14 @@
+package br.com.guiabolso.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PokemonProfessorTest {
+    
+    @Test
+    public void test() {
+        fail("no test");
+    }
+
+}
